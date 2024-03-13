31fb2c7a555a461b62ab34da77b6b59419f7ab26
Dockins207 <otienodockins48@gmail.com>
2024-03-13 08:59:58 -0700

    done

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,7 +9,12 @@ def top_ten(subreddit):
     headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
     params = {"limit": 10}
 
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(
+            url,
+            headers=headers,
+            params=params,
+            allow_redirects=False
+            )
 
     if response.status_code == 200:
         results = response.json().get("data", {}).get("children", [])
