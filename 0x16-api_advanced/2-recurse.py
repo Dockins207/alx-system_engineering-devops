08306ff8bb3f016e1218df5a822211a465c45f5c
Dockins207 <otienodockins48@gmail.com>
2024-03-13 07:58:00 -0700

    done

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
 """Function to query a list of all hot posts on a given Reddit subreddit."""
 import requests
-
 def recurse(subreddit, hot_list=None, after="", count=0):
     """Returns a list of titles of all hot posts on a given subreddit."""
     if hot_list is None:
         hot_list = []
-    
-    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
-    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
-    params = {"after": after, "count": count, "limit": 100}
-    
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-    if response.status_code != 200:
-        return None  # You might want to handle different status codes differently
-    
-    results = response.json().get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    
-    for post in results.get("children", []):
-        hot_list.append(post.get("data", {}).get("title"))
-    
-    if after is not None:
-        return recurse(subreddit, hot_list, after, count)
-    return hot_list
 
+        url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+        headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
+        params = {"after": after, "count": count, "limit": 100}
+
+        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+        if response.status_code != 200:
+            return None  # You might want to handle different status codes differently
+
+        results = response.json().get("data")
+        after = results.get("after")
+        count += results.get("dist")
+
+        for post in results.get("children", []):
+            hot_list.append(post.get("data", {}).get("title"))
+
+            if after is not None:
+                return recurse(subreddit, hot_list, after, count)
+            return hot_list
