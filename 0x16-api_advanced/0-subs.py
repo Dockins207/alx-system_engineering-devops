d59d82c0b801aca6618a30ec877f5926aaa53d97
Dockins <129792373+Dockins207@users.noreply.github.com>
2024-04-01 10:32:16 -0400

    done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
-'''
-    this module contains the function number_of_subscribers
-'''
+"""Python module that queries the Reddit API and returns the
+number of sunscribers for a given subreddit
+"""
+
 import requests
-from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    '''
-        returns the number of subscribers for a given subreddit
-    '''
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/about.json'
-                       .format(subreddit), headers=user).json()
-    try:
-        return url.get('data').get('subscribers')
-    except Exception:
+    """Obtains number of subscribers in a parsed
+    subreddit"""
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    # Setting up a custom User-Agent to avoid too many Request errors
+    headers = {'User-Agent': 'Google Chrome Version 120.0.6099.217'}
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        output = response.json()
+        number_of_subscribers = output['data']['subscribers']
+        return (number_of_subscribers)
+    else:
         return 0
-
-
-if __name__ == "__main__":
-    number_of_subscribers(argv[1])
